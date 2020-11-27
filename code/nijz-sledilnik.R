2527b68064d14a8ffc27c4fe467d703caaf46c03
mz <maja.zaloznik@gmail.com>
2020-11-27 01:13:56 +0100

    update NIJZ vs gov data charts, add cumulative

--- code/nijz-sledilnik.R
+++ code/nijz-sledilnik.R
@@ -4,6 +4,7 @@
 library(readr)
 library(readxl)
 library(dplyr)
+library(tidyr)
 
 ###############################################################################
 #### import data ##############################################################
@@ -21,24 +22,24 @@ download.file(paste0("https://raw.githubusercontent.com/",
 
 # NIJZ daily death for 3 different timestamps
 # manually changed text to date in table 12.11....
-# download.file("https://www.nijz.si/sites/www.nijz.si/files/uploaded/porocilo_stevilo_potrjenih_primerov_covid-19_12112020_splet.xlsx",
-#               "data/12.11.xlsx")
-# download.file("https://www.nijz.si/sites/www.nijz.si/files/uploaded/porocilo_stevilo_potrjenih_primerov_covid-19_05112020_splet.xlsx",
-#               "data/5.11.xlsx")
-# download.file("https://www.nijz.si/sites/www.nijz.si/files/uploaded/porocilo_stevilo_potrjenih_primerov_covid-19_29102020_splet.xlsx",
-#               "data/29.10.xlsx")
+download.file("https://www.nijz.si/sites/www.nijz.si/files/uploaded/porocilo_stevilo_potrjenih_primerov_covid-19_25112020_splet.xlsx",
+              "data/25.11.xlsx")
+download.file("https://www.nijz.si/sites/www.nijz.si/files/uploaded/porocilo_stevilo_potrjenih_primerov_covid-19_18112020_splet.xlsx",
+              "data/18.11.xlsx")
+download.file("https://www.nijz.si/sites/www.nijz.si/files/uploaded/porocilo_stevilo_potrjenih_primerov_covid-19_11112020_splet.xlsx",
+              "data/11.11.xlsx")
 
 # import 
 sledilnik <- read_csv("data/stats.csv")
 nijz.regions <- read_csv("data/deceased-regions.csv")
-nijz12.11 <- read_excel("data/12.11.xlsx", sheet = "tb6", range = "A3:U115",
+nijz25.11 <- read_excel("data/25.11.xlsx", sheet = "tb6", range = "A3:U128",
                         col_types = c("date", rep("numeric", 20)))
 
-nijz05.11 <- read_excel("data/5.11.xlsx", sheet = "tb6", range = "A3:V108",
-                        col_types = c("date", rep("numeric", 21)))
+nijz18.11 <- read_excel("data/18.11.xlsx", sheet = "tb6", range = "A3:U121",
+                        col_types = c("date", rep("numeric", 20)))
 
-nijz29.10 <- read_excel("data/29.10.xlsx", sheet = "tb6", range = "A3:S101",
-                        col_types = c("date", rep("numeric", 18)))
+nijz11.11 <- read_excel("data/11.11.xlsx", sheet = "tb6", range = "A3:U114",
+                        col_types = c("date", rep("numeric", 20)))
 
 ###############################################################################
 #### clean up #################################################################
@@ -53,32 +54,39 @@ nijz.regions %>%
          nijz = c(total[1], diff(total))) %>% 
   select(date, nijz)-> nijz.regions
 
-nijz12.11 %>%  
+nijz25.11 %>%  
   rename("date" = `...1`,
-         "no3" = `SKUPAJ...21`) %>% 
-  mutate(date = as.Date(date)) %>% 
-  select(date, no3) -> no3
+         "no3" = `Skupaj`) %>% 
+  mutate(date = as.Date(date),
+         no3.cum = cumsum(no3)) %>% 
+  select(date, no3, no3.cum) -> no3
 
-nijz05.11 %>%  
+nijz18.11 %>%  
   rename("date" = `...1`,
-         "no2" = `SKUPAJ...22`) %>% 
-  mutate(date = as.Date(date)) %>% 
-  select(date, no2) -> no2
+         "no2" = `SKUPAJ...21`) %>% 
+  mutate(date = as.Date(date),
+         no2.cum = cumsum(no2)) %>% 
+  select(date, no2, no2.cum) -> no2
 
-nijz29.10 %>%  
+nijz11.11 %>%  
   rename("date" = `...1`,
-         "no1" = `SKUPAJ...19`) %>% 
-  mutate(date = as.Date(date)) %>% 
-  select(date, no1) -> no1
+         "no1" = `SKUPAJ...21`) %>% 
+  mutate(date = as.Date(date),
+         no1.cum = cumsum(no1)) %>% 
+  select(date, no1, no1.cum) -> no1
 
 #### join tables ##############################################################
 df %>%  left_join(nijz.regions) %>% 
   left_join(no1) %>% 
   left_join(no2) %>% 
-  left_join(no3) %>% 
-  mutate(no3 = replace(no3, is.na(no3) & date <= as.Date("2020-10-28"), 0))   %>% 
-  mutate(no2 = replace(no2, is.na(no2) & date <= as.Date("2020-10-28"), 0))  %>% 
-  mutate(no1 = replace(no1, is.na(no1) & date <= as.Date("2020-10-28"), 0))  -> df
+  left_join(no3)  %>% 
+  fill(no1.cum, no2.cum, no3.cum) %>% 
+  mutate(no1.cum = ifelse(date > "2020-11-11", NA, no1.cum),
+         no2.cum = ifelse(date > "2020-11-18", NA, no2.cum),
+         no3.cum = ifelse(date > "2020-11-25", NA, no3.cum), 
+         no1 = ifelse(date < "2020-11-11", replace_na(no1, 0), no1),
+         no2 = ifelse(date < "2020-11-18", replace_na(no2, 0), no2),
+         no3 = ifelse(date < "2020-11-25", replace_na(no3, 0), no3)) -> df
 
 ###############################################################################
 #### plot cumulative gov vs NIJZ ##############################################
@@ -139,13 +147,14 @@ mtext(side = 1, line = 2.5,   cex = 1, "datum")
 
 
 #### plot daily NIJZ entry vs gov #####################################################
+# why doesnt' this work anymore? 26.11.
 # png(filename="figures/dailyNIJZ.png", 800, 480)
 par(mar = c(5, 5, 4, 0.5) + 0.1)
 plot(df$date, df$no3, type = "l",
      xlab = "",
      ylab = "",
      ylim = c(0,40),
-     xlim = as.Date(c("2020-07-13", "2020-11-11")),
+     xlim = as.Date(c("2020-07-13", "2020-11-25")),
      axes = FALSE, 
      lty = 1, lwd = 2, col = "black")
 axis.Date(1, at = seq(df$date[1], df$date[length(df$date)]+10, by = "2 week"),
@@ -196,36 +205,73 @@ mtext(side = 1, line = 2.5,   cex = 1, "datum")
 
 
 #### plot daily NIJZ three tables + gov #######################################
-# png(filename="figures/threeNIJZgov.png", 800, 480)
-par(mar = c(5, 5, 4, 0.5) + 0.1)
-plot(df$date, df$no1, type = "l",
+png(filename="figures/threeNIJZgov.png", 800, 480)
+ par(mar = c(5, 5, 2, 0.5) + 0.1)
+ plot(df$date, df$no1, type = "l",
      xlab = "",
      ylab = "",
-     ylim = c(0,40),
-     xlim = as.Date(c("2020-10-01", "2020-11-15")),
+     ylim = c(0,60),
+     xlim = as.Date(c("2020-09-10", "2020-11-26")),
      axes = FALSE, 
-     lty = 1, lwd = 2, col = "orange")
+     lty = 1, lwd = 2, col = "blue")
 axis.Date(1, at = seq(df$date[1], df$date[length(df$date)]+10, by = "2 week"),
           format = "%d.%m.")
 
 axis(2, las = 2)
 
-lines(df$date, df$no2, col = "red", lwd = 2, lty = 1)
+lines(df$date, df$no2, col = "blue4", lwd = 2, lty = 1)
 lines(df$date, df$no3, col = "black", lwd = 2, lty = 1)
-lines(df$date, df$deceased.gov, col = "gray", lwd = 2, lty = 5)
+lines(df$date, df$deceased.gov, col = "red", lwd = 2, lty = 5)
 
-legend(as.Date("2020-10-01"), 20,
-       legend=rev(c("Število umrlih do 28.10. (NIJZ)", "Število umrlih do 4.11. (NIJZ)",
-                "Število umrlih do 11.11. (NIJZ)", "Število umrlih (vlada)")),
-       col=rev(c("orange", "red", "black", "gray")), lty=rev(c(1,1,1, 5)),
+legend(as.Date("2020-09-15"), 50,
+       legend=rev(c("Število umrlih do 11.11. (NIJZ)", "Število umrlih do 18.11. (NIJZ)",
+                "Število umrlih do 25.11. (NIJZ)", "Število umrlih (vlada)")),
+       col=rev(c("blue", "blue4", "black", "red")), lty=rev(c(1,1,1, 5)),
        cex=0.9, bty = "n", lwd = 2)
-mtext(side = 3, line = 2.5,  adj = 0, cex = 1.1,
-      "Primerjava dnevnega števila umrlih vnešenih v NIJZ tabele na različne dni")
-mtext(side = 3, line = 1,  adj = 0, cex = 1.1,
-      "IN 'TA PRAVIH' PODATKOV")
+mtext(side = 3, line = 0.5,  adj = 0, cex = 1.1,
+      "Primerjava dnevnega števila umrlih vnešenih v NIJZ tabele na različne dni in vladnih podatkov (rdeča)")
+
+# mtext(side = 3, line = 1,  adj = 0, cex = 1.1,
+#       "in vladnih podatkov (rdeča)")
 mtext(side = 2, line = 3,   cex = 1,"dnevno število umrlih")
 mtext(side = 1, line = 2.5,   cex = 1, "datum")
-# dev.off()
+dev.off()
+
+
+
+
+#### plot kumulative NIJZ three tables + gov#######################################
+png(filename="figures/threeNIJZgov-cum.png", 800, 480)
+par(mar = c(5, 5, 2, 0.5) + 0.1)
+plot(df$date, df$no1.cum, type = "l",
+     xlab = "",
+     ylab = "",
+     ylim = c(0,1300),
+     xlim = as.Date(c("2020-09-10", "2020-11-26")),
+     axes = FALSE, 
+     lty = 1, lwd = 2, col = "blue")
+axis.Date(1, at = seq(df$date[1], df$date[length(df$date)]+10, by = "2 week"),
+          format = "%d.%m.")
+
+axis(2, las = 2)
+
+lines(df$date, df$no2.cum, col = "blue4", lwd = 2, lty = 1)
+lines(df$date, df$no3.cum, col = "black", lwd = 2, lty = 1)
+lines(df$date, df$state.deceased.todate, col = "red", lwd = 2, lty = 5)
+
+legend(as.Date("2020-09-15"), 600,
+       legend=rev(c("Število umrlih do 11.11. (NIJZ)", "Število umrlih do 18.11. (NIJZ)",
+                    "Število umrlih do 25.11. (NIJZ)", "Število umrlih (vlada)")),
+       col=rev(c("blue", "blue4", "black", "red")), lty=rev(c(1,1,1, 5)),
+       cex=0.9, bty = "n", lwd = 2)
+mtext(side = 3, line = 0.5,  adj = 0, cex = 1.1,
+      "Primerjava skupnega števila umrlih vnešenih v NIJZ tabele do treh različnih datumov in vladnih podatkov (rdeča)")
+# mtext(side = 3, line = 1,  adj = 0, cex = 1.1,
+#       "")
+mtext(side = 2, line = 3,   cex = 1,"skupno število umrlih")
+mtext(side = 1, line = 2.5,   cex = 1, "datum")
+dev.off()
+
 
 
 
