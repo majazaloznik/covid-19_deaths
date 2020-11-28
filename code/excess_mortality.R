16741542348f5e927965f94e6c72044a2c0212bd
mz <maja.zaloznik@gmail.com>
2020-11-28 01:50:46 +0100

    clean up excess deaths

--- code/excess_mortality.R
+++ code/excess_mortality.R
@@ -5,11 +5,10 @@ library(readr)
 library(tidyr)
 library(dplyr)
 library(lubridate) 
-# library(ggplot2)
-# library(forecast)
 
 ###############################################################################
 #### import data ##############################################################
+###############################################################################
 
 # # uncomment to download
 # # sledilnik = NIJZ daily death data from region table tb5
@@ -64,6 +63,9 @@ left_join(df.5, covid)  %>%
   
 ###############################################################################
 ## plot 
+###############################################################################
+# inspiration: https://www.economist.com/graphic-detail/2020/07/15/tracking-covid-19-excess-deaths-across-countries
+# https://github.com/TheEconomist/covid-19-excess-deaths-tracker
 
 png(filename="figures/ecxcess-15-19-baseline.png", 800, 480)
 par(mar = c(4, 4, 4, 1.5) + 0.1)
@@ -98,110 +100,3 @@ legend(1.25, 40,
 
 dev.off()
 
-
-
-
-# 
-# 
-# 
-# ## old charts
-# 
-# 
-# group = c(rep(1, 43), rep(2, 1))
-# deaths <- ts(monthly$daily, start=c(1977, 1), end=c(2020, 10), frequency=12)
-# 
-# 
-# greyz <- rev(paste0("gray", 26:(26+42)))
-# colz <-c( rev(paste0("gray", 26:(26+42))), "red")
-# 
-# 
-# monthly %>% 
-#   select(-value) %>% 
-#   spread(year, daily) %>% 
-#   mutate(Means = rowMeans(.[,2:43])) -> df
-# 
-# 
-# 
-# 
-# #square function
-# sq<-function(x){
-#   x^2
-# }
-# #inverse square function (square root)
-# isq<-function(x){
-#   print(paste("isq",x))  #debug statement
-#   x<-ifelse(x<0, 0, x)
-#   sqrt(x)
-# }
-# 
-# ###############################################################################
-# deaths2  <- window(deaths, start=c(1993, 1), end=c(1995, 1))
-# 
-# ggseasonplot(deaths2,  polar=TRUE) +
-#   scale_y_continuous(
-#     limits = c(0, 70),
-#     trans=scales::trans_new("sq", sq, isq)) 
-# 
-# ggseasonplot(deaths,  polar=TRUE, size = c(rep(1, 43), rep(2, 1))) +
-#   theme_bw() +
-#   theme(legend.position = "none")  +
-#   scale_colour_manual(values=c(rep("gray",43), "red")) 
-# 
-# 
-# ggseasonplot(deaths, polar = TRUE) +
-#   theme_bw() +
-#   scale_y_continuous(
-#     limits = c(0, 80)) +
-#   theme(legend.position = "none")  +
-#   scale_colour_manual(values=c(greyz, "red")) 
-# 
-# 
-# 
-# ggseasonplot(deaths,  polar=TRUE, size = c(rep(1, 43), rep(2, 1))) +
-#   theme_bw() +
-#   #geom_line( alpha = c(rep(0.5, 559), rep(1, 10))) +
-#   # geom_path(size = c(rep(0.5, 559), rep(3, 10))) +
-#   scale_y_continuous(
-#     limits = c(0, max(deaths)),
-#    trans=scales::trans_new("sq", sq, isq)) +
-#   theme(legend.position = "none")  +
-#   scale_colour_manual(values=c(greyz, "red")) 
-# 
-# ###############################################################################
-# 
-# plot(1:12, rep(0,12), ylim = c(0,80), type = "n",
-#      axes = FALSE,
-#      xlab = "",
-#      ylab = "")
-# axis(1, at = 1:12, labels = month.abb)
-# axis(2, las =2)
-# 
-# for (i in 2:44){
-#   lines(1:12, df[[i]] , col = colz[i-1], type = 'l')
-# }
-# lines(1:12, df[[45]] , col = colz[44], type = 'l', lwd = 2)
-# arrows(10, pull(df[10,45]),10, pull(df[10,45])-188/31, col = "blue", lwd = 2, code = 3,
-#               length=0.07,angle=30)
-# 
-# ###############################################################################
-# 
-# 
-# plot(1:12, rep(0,12), ylim = c(0,80), type = "n",
-#      axes = FALSE,
-#      xlab = "",
-#      ylab = "")
-# axis(1, at = 1:12, labels = month.abb)
-# axis(2, las =2)
-# 
-# for (i in 2:44){
-#   lines(1:12, df[[i]] , col = "gray",  type = 'l')
-# }
-# lines(1:12, df[[46]] , col = "gray30", type = 'l', lwd = 3)
-# 
-# arrows(10, pull(df[10,45]),10, pull(df[10,45])-188/31, col = "black", lwd = 1.5, code = 3,
-#        length=0.07,angle=90)
-# 
-# lines(1:12, df[[45]] , col = colz[44], type = 'l', lwd = 2.5)
-# 
-# 
-#       
